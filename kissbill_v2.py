22844a4ae3182ebb8a8269625f3a32c44f29c3c6
Unicornssss7428 <tamalito@email.com>
2024-09-22 16:54:47 -0400

    Added more functionality to kissbillv2, soon to replace kissbill.

--- kissbill_v2.py
+++ kissbill_v2.py
@@ -9,6 +9,11 @@ class Fund:
     def __init__(self, balance):
         self.balance = balance
 
+def total_funds(funds):
+    total_funds = 0
+    for fund in funds:
+        total_funds += fund.balance
+    return total_funds
 def get_bills(file):
       with open(file) as f:
           toml_bills = toml.load(f)
@@ -41,13 +46,27 @@ def total_pay(parsed_bills, start=1, end=31):
             total_pay += event.amount 
     return total_pay
 
-# How much can I spend each day/week/month and still pay for expenses/save for emergency fund, car, etc.
-def total_spending_power(parsed_bills, funds, option):
-    pass
-
 # Calculate how much I need to save per day/week/month to pay for expenes and reach funds goals.
-def total_need_saved(parsed_bills, funds, option):
-    pass
+def total_need_saved(parsed_bills, funds, option='m'):
+   bills_total = total_bills(parsed_bills) 
+   funds_total = total_funds(funds)
+   total_expenses = bills_total + funds_total
+   # Over 31 days, how much money does it take to make total_expenses?
+   # 31x = total_expenses
+   # x = total_expenses / 31
+   return total_expenses / 31
+   
+def total_can_save(parsed_bills):
+    bills_total = total_bills(parsed_bills)
+    pay_total = total_pay(parsed_bills)
+    return pay_total - bills_total
+
+  # How much can I spend each day/week/month and still pay for expenses/save for emergency fund, car, etc.
+def total_spending_power(parsed_bills, funds, option='m'):                                      
+    bills_total = total_bills(parsed_bills)
+    funds_total = total_funds(funds)
+    total_expenses = bills_total + funds_total
+    
 
 # Calculate how much a certain financial decision on a certain date (due) effects ability to pay for expenses and save for funds.
 def if_spend(parsed_bills, funds, due):
@@ -56,6 +75,4 @@ def if_spend(parsed_bills, funds, due):
 # Display a "calendar" that tells me how much money I can spend on each day of the month while still being able to pay for my expenses and funds.
 def calendar(parsed_bills, funds):
     pass
-
-
 
--- test.py
+++ test.py
@@ -2,4 +2,8 @@ from kissbill_v2 import *
 
 bills_file = get_bills('bills.toml')
 parsed_bills = parse_bills(bills_file)
-print(total_pay(parsed_bills))
+print(total_bills(parsed_bills)*6)
+emergency_fund = Fund(total_bills(parsed_bills)*6)
+
+print(total_can_save(parsed_bills))
+print(total_need_saved(parsed_bills, [emergency_fund]))
